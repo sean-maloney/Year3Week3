a9ace4ce2d08e80d62e6e6dabc6599a744db5894
G00420811@atu.ie <G00420811@atu.ie>
2024-10-08 10:15:44 +0100

    Initial init

---
+++ src/test/java/ie/atu/week3/productapp/Week3ApplicationTests.java
@@ -0,0 +1,13 @@
+package ie.atu.week3.productapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Week3ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
